99a44bd27407a1afc2ce1c7f81fdcad3085c02bb
piramid <pornchai.tom@gmail.com>
2022-12-05 08:17:01 +0700

    Add files via upload

---
+++ lilygoLine.ino
@@ -0,0 +1,41 @@
+#include <TridentTD_LineNotify.h>
+
+#define SSID        "Guitar-2.4"                                     //ใส่ ชื่อ Wifi ที่จะเชื่อมต่อ
+#define PASSWORD    "AAAAABBBB1"                                   //ใส่ รหัส Wifi
+#define LINE_TOKEN  "GV9rF1DbQMChekhqzMtd0XZzPwvFS79Lx4L9sNHAlyN" //ใส่ รหัส TOKEN ที่ได้มาจากข้างบน
+
+void setup() {
+  Serial.begin(115200); Serial.println();
+  Serial.println(LINE.getVersion());
+
+  WiFi.begin(SSID, PASSWORD);
+  Serial.printf("WiFi connecting to %s\n",  SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(400);
+  }
+  Serial.printf("\nWiFi connected\nIP : ");
+  Serial.println(WiFi.localIP());
+
+  // กำหนด Line Token
+  LINE.setToken(LINE_TOKEN);
+
+  // ตัวอย่างส่งข้อความ
+  LINE.notify("ยินดีต้อนรับ");
+  LINE.notify("Cybertice.com");
+  // ตัวอย่างส่งข้อมูล ตัวเลข
+  LINE.notify(2342);          // จำนวนเต็ม
+  LINE.notify(212.43434, 5);  // จำนวนจริง แสดง 5 หลัก
+
+  // เลือก Line Sticker ได้จาก https://devdocs.line.me/files/sticker_list.pdf
+  LINE.notifySticker(3, 240);       // ส่ง Line Sticker ด้วย PackageID 3 , StickerID 240
+  LINE.notifySticker("Hello", 1, 2); // ส่ง Line Sticker ด้วย PackageID 1 , StickerID 2  พร้อมข้อความ
+
+  // ตัวอย่างส่ง รูปภาพ ด้วย url
+  LINE.notifyPicture("https://preview.ibb.co/j6G51n/capture25610417181915334.png");
+  LINE.notifyPicture("จตุธาตุ", "https://www.fotoaparat.cz/storage/pm/09/10/23/670915_a5351.jpg");
+}
+
+void loop() {
+  delay(1);
+}
